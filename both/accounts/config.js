1a7c5f24c66c5cb4f45b2801379cf883769fd6e2
Adil Haritah <haritahadil@yahoo.fr>
2015-07-16 17:42:22 +0000

    fix signup redirect

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -1,4 +1,9 @@
-AccountsTemplates.configureRoute('signIn', {layoutTemplate: 'appLayout', redirect: '/'});
-AccountsTemplates.configureRoute('signUp', {layoutTemplate: 'appLayout'});
+AccountsTemplates.configureRoute('signIn', {
+  layoutTemplate: 'appLayout',
+  redirect: '/'
+});
+AccountsTemplates.configureRoute('signUp', {
+  layoutTemplate: 'appLayout',
+  redirect: '/'
+});
 AccountsTemplates.configureRoute('ensureSignedIn', {layoutTemplate: 'appLayout'});
-
